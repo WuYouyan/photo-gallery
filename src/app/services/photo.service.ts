473c6aaa5991591d22ae3e4277a380f61c816a8e
Youyan WU <wuyouyan1@gmail.com>
2020-06-07 15:53:50 +0200

    [First App] Loading Photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -14,6 +14,7 @@ interface Photo {
 })
 export class PhotoService {
   public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = "photos";
 
   constructor() { }
 
@@ -64,6 +65,33 @@ export class PhotoService {
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto)
     this.photos.unshift(savedImageFile);
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos.map(p => {
+        // Don't save the base64 representation of the photo data, 
+        // since it's already saved on the Filesystem
+        const photoCopy = { ...p };
+        delete photoCopy.base64;
+        return photoCopy;
+      }))
+    });
+  }
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photos = await Storage.get({key: this.PHOTO_STORAGE});
+    this.photos = JSON.parse(photos.value) || [];
+
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: FilesystemDirectory.Data
+      });
+      // Web platform only: Save the photo into the base64 field
+      photo.base64 = `data:imgage/jpeg;base64,${readFile.data}`;
+    }
   }
 
 }
